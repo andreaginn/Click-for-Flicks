1c4ca8b671c9e8fbc6cd62155dcdbfefdb3bb1ce
Wendy Vu <wendyvuhoo@gmail.com>
2023-02-09 19:50:22 -0800

    deleted duplications

--- assets/script.js
+++ assets/script.js
@@ -4,34 +4,6 @@ const watchLinkEl = document.getElementById("icon");
 const newMovie = document.getElementById("show-another");
 
 
-generateBtn.addEventListener("click", function () {
-  var IMDBKey = 'k_vvb7ncfd';
-  var IMDBURL = 'https://imdb-api.com/en/API/Top250Movies/' + IMDBKey;
-  var WatchModeKey = 'tRrVcKsz5MC88YyphjWCppQ9y6boLgUcE0STBETT';
-  var WatchModeURL = 'https://api.watchmode.com/v1/title/345534/sources/?apiKey=' + WatchModeKey;
-
-  newMovie.style.display = "block";
-
-  fetch(IMDBURL)
-    .then(response => response.json())
-    .then(data => {
-      for (let i = 0; i < data.items.length; i++) {
-        movieNameEl.textContent = data;
-        console.log(data.items[i].length)
-      }
-    });
-
-  fetch(WatchModeURL)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    });
-
-  //randomization of movies??
-
-
-});
-
 generateBtn.addEventListener("click", function() {
 var IMDBKey = 'k_vvb7ncfd'; 
 var IMDBURL = 'https://imdb-api.com/en/API/Top250Movies/'+IMDBKey;
@@ -39,6 +11,8 @@ var WatchModeKey = 'tRrVcKsz5MC88YyphjWCppQ9y6boLgUcE0STBETT';
 //var WatchModeURL = 'https://api.watchmode.com/v1/networks/?apiKey=' + WatchModeKey;
 var IMDBID = '';
 var WatchModeURL = 'https://api.watchmode.com/v1/title/' + IMDBID + '/details/?apiKey=' + WatchModeKey + '&append_to_response=sources';
+
+newMovie.style.display = "block";
 
     fetch(IMDBURL)
       .then(response => response.json())
