2f823458dbf2c6fa0911a6dfc619451be54dc804
Wendy Vu <wendyvuhoo@gmail.com>
2023-02-09 19:45:33 -0800

    updated API

--- assets/script.js
+++ assets/script.js
@@ -6,23 +6,26 @@ generateBtn.addEventListener("click", function() {
 var IMDBKey = 'k_vvb7ncfd'; 
 var IMDBURL = 'https://imdb-api.com/en/API/Top250Movies/'+IMDBKey;
 var WatchModeKey = 'tRrVcKsz5MC88YyphjWCppQ9y6boLgUcE0STBETT';
-var WatchModeURL = 'https://api.watchmode.com/v1/networks/?apiKey=' + WatchModeKey;
+//var WatchModeURL = 'https://api.watchmode.com/v1/networks/?apiKey=' + WatchModeKey;
+var IMDBID = '';
+var WatchModeURL = 'https://api.watchmode.com/v1/title/' + IMDBID + '/details/?apiKey=' + WatchModeKey + '&append_to_response=sources';
 
     fetch(IMDBURL)
       .then(response => response.json())
       .then(data => {
         for (let i = 0; i< data.items.length; i++) {  
-        console.log(data.items[i].title) 
+        //console.log(data) 
         movieNameEl.textContent = data.items[i].title;
         }
       });
       
+      
     fetch(WatchModeURL)
       .then(response => response.json())
       .then(data => {
-      for (let i = 0; i< data.length; i++) {
-      console.log(data[i].name);
-      }
+      //for (let i = 0; i< data.length; i++) {
+      console.log(data);
+      //}
     });
     
     //randomization of movies??
