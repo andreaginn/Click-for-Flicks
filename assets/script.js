0ea68488050a59981d64edeec32d303b9409c0a2
Wendy Vu <wendyvuhoo@gmail.com>
2023-02-13 20:46:31 -0800

    made randomization work

--- assets/script.js
+++ assets/script.js
@@ -4,42 +4,45 @@ const watchLinkEl = document.getElementById("link");
 const newMovie = document.getElementById("show-another");
 
 
+
+
 generateBtn.addEventListener("click", function() {
 
-var IMDBKey = 'k_vvb7ncfd'; 
+//var IMDBKey = 'k_vvb7ncfd'; 
+var IMDBKey = 'k_6i8t9r8e';
 var IMDBURL = 'https://imdb-api.com/en/API/Top250Movies/'+IMDBKey;
 var WatchModeKey = 'tRrVcKsz5MC88YyphjWCppQ9y6boLgUcE0STBETT';
-//var WatchModeURL = 'https://api.watchmode.com/v1/networks/?apiKey=' + WatchModeKey;
-var IMDBID = 'tt0111161' ;
+var IMDBID = '' ;
 var WatchModeURL = 'https://api.watchmode.com/v1/title/' + IMDBID + '/details/?apiKey=' + WatchModeKey + '&append_to_response=sources';
+
+//var WatchModeURL = 'https://api.watchmode.com/v1/networks/?apiKey=' + WatchModeKey;
+
 
 newMovie.style.display = "block";
 
     fetch(IMDBURL)
       .then(response => response.json())
       .then(data => {
-        console.log(data);
-        for (let i = 0; i< data.items.length; i++) {  
-        console.log(data) 
-        movieNameEl.textContent = data.items[i].title;
-        IMDBID = data.items[i].id;
-      }
-    });
-      
+        //console.log(data);
+        var movieNames = data.items
+        var randomMovie = movieNames[(Math.floor(Math.random() * data.items.length))];
+        console.log(randomMovie.title);
+        console.log(randomMovie.id);
+        movieNameEl.textContent = randomMovie.title;
+        IMDBID = randomMovie.id;
+        WatchModeURL = 'https://api.watchmode.com/v1/title/' + IMDBID + '/details/?apiKey=' + WatchModeKey + '&append_to_response=sources';
 
-    fetch(WatchModeURL)
+      //secondAPI
+      console.log(WatchModeURL)
+      fetch(WatchModeURL)
       .then(response => response.json())
       .then(data => {
-      for (let i = 0; i< 5; i++) {
-      console.log(data.sources[i].web_url);
-      watchLinkEl.textContent = data.sources[i].web_url;
-      }
+        //console.log(data);
+        for (let i = 0; i< 5; i++) {
+          watchLinkEl.textContent = data.sources[i].web_url;
+          }
+
+      });
     });
     
-    //randomization of movies??
-    
-    
   });
-
-
-
